87c68c81f3a920140fdf4901429379b7add69a9c
cYRUS cHOI <77631273+cyruschoisy@users.noreply.github.com>
2026-01-31 23:04:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/submit': 'http://localhost:3000',
+      '/api': 'http://localhost:3000',
+      '/receipts': 'http://localhost:3000'
+    }
+  }
 })
