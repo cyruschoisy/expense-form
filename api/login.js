e0ee6628a45722d9009abfde6e2d4978f9da0841
cYRUS cHOI <77631273+cyruschoisy@users.noreply.github.com>
2026-02-01 01:08:01 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -14,7 +14,7 @@ function renderLogin(errorCode) {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="bg-light">
-      <div class="container py-5">
+      <div class="container-fluid p-5">
         <div class="row justify-content-center">
           <div class="col-md-4">
             <div class="card shadow-sm">
